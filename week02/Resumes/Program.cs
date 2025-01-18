cc4dbbe3fffe4cc478ab32f3b18617de1f61b490
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-01-18 10:10:30 +0100

    Resume.cs added

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create first job instance
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create second job instance
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume instance
+        Resume myResume = new Resume();
+        myResume._name = "Oluwayemi Oni";
+
+        // Add jobs to the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display resume details
+        myResume.Display();
     }
 }
