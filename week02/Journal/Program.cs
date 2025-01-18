ac37bd74d5d3a0e9371c8c5be2213d234b56f627
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-01-18 13:19:33 +0100

    Program.cs added

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -3,7 +3,48 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+  {
+        Journal journal = new Journal();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save the journal to file");
+            Console.WriteLine("4. Load  the journal from file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    journal.WriteNewEntry();
+                    break;
+                case "2":
+                    journal.DisplayJournal();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to save: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveToFile(saveFile);
+                    break;
+                case "4":
+                    Console.Write("Enter filename to load: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadFromFile(loadFile);
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, please try again.");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Goodbye!");
     }
 }
