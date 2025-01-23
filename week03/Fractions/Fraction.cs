02a26c82d3dcf6dfb2bc3c9c5097419afe6489db
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-01-23 14:02:16 +0100

    Fraction.cs added

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,68 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        // Default to 1/1
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom !=0 ? bottom : 1;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            Console.WriteLine("Bottom cannot be zero. Keeping the current value.");
+        }
+    }
+
+    public string GetFractionString()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is is just a temporary, local variable that will be recomputed each time this is called.
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is will be recomputed each time this is called.
+        return (double)_top / (double)_bottom;
+    }
+}
