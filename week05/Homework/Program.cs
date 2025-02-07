83ce0d95cdbc99bf41499fc3ce7ba6bcf8281819
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-07 01:00:53 +0100

    Program.cs added

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+       // Testing Assignment class
+        Assignment generalAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(generalAssignment.GetSummary());
+
+        // Testing MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        // Testing WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
