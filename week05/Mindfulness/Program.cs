900f1e5025c694187e45cb94df87f21a94d83502
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-09 22:14:44 +0100

    Program.cs added

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+ while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Mindfulness Activities");
+            Console.WriteLine("1. Breathing Exercise");
+            Console.WriteLine("2. Reflection Exercise");
+            Console.WriteLine("3. Listing Exercise");
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    new BreathingActivity().Start();
+                    break;
+                case "2":
+                    new ReflectingActivity().Start();
+                    break;
+                case "3":
+                    new ListingActivity().Start();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Try again.");
+                    break;
+            }
+        }
     }
 }
