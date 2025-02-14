56a74e86af2e8a9f9ee472f629ca144a0cf4eff4
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-14 18:28:09 +0100

    Program.cs added

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+      List<Shape> shapes = new List<Shape>
+        {
+            new Square("Yellow", 5),
+            new Rectangle("Blue", 4, 6),
+            new Circle("Red", 3)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
     }
 }
