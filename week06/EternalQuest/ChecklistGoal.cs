ef091be92b2f29c66e4031ce66db23d0cf820688
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-16 01:41:56 +0100

    ChecklistGoal.cs added

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,34 @@
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    public int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus)
+        : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+    }
+
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"[✔ {_amountCompleted}/{_target}] {_shortName}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal|{_shortName}|{_description}|{_points}|{_amountCompleted}|{_target}|{_bonus}";
+    }
+}
