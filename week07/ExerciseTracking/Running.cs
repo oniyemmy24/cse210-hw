03e5e2b6ba6d72eb73ca6bfe083b6672b5b7598c
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-19 08:02:48 +0100

    Running.cs

---
+++ week07/ExerciseTracking/Running.cs
@@ -0,0 +1,14 @@
+class Running : Activity
+{
+    private double distance; // in km
+
+    public Running(DateTime date, int duration, double distance)
+        : base(date, duration)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance() => distance;
+    public override double GetSpeed() => (distance / GetDuration()) * 60;
+    public override double GetPace() => GetDuration() / distance;
+}
