d9918de63f040d669ba10cd344373874af870178
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-19 08:02:31 +0100

    Cycling.cs

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,14 @@
+class Cycling : Activity
+{
+    private double speed; // in kph
+
+    public Cycling(DateTime date, int duration, double speed)
+        : base(date, duration)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance() => (speed * GetDuration()) / 60;
+    public override double GetSpeed() => speed;
+    public override double GetPace() => 60 / speed;
+}
