7749d0ca626a26b56c7d4512d35f3d0ecacd580e
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-02-19 08:03:28 +0100

    Program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+    DateTime currentDate = DateTime.Today;
+    List<Activity> activities = new List<Activity>
+        {
+            new Running(currentDate, 30, 4.8),
+            new Cycling(currentDate, 45, 20.0),
+            new Swimming(currentDate, 25, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
